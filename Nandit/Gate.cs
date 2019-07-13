139c5cd21f3e68133be32fc9a9d21a393b161403
Zakkery Diaz <zakkdiaz@hotmail.com>
2019-07-12 23:16:59 -0700

    fixed filenames

---
+++ Nandit/Gate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Nandit
+{
+    public class Gate
+    {
+        public int Id;
+        public bool Value = false;
+        public Gate(int i)
+        {
+            this.Id = i;
+        }
+
+        public void Flip()
+        {
+            Value = !Value;
+        }
+
+        public void Set(bool v)
+        {
+            Value = v;
+        }
+    }
+}
--- Nandit/GateLink.cs
+++ Nandit/GateLink.cs
@@ -2,23 +2,44 @@
 
 namespace Nandit
 {
-    public class Gate
+    public class GateLink
     {
+
+        private Gate gate1;
+        private Gate gate2;
         public int Id;
-        public bool Value = false;
-        public Gate(int i)
+        private GateLink _from;
+        private GateLink _from2;
+        private Gate c;
+
+        public GateLink(int id, Gate gate1, Gate gate2)
         {
-            this.Id = i;
+            Id = id;
+            this.gate1 = gate1;
+            this.gate2 = gate2;
         }
 
-        public void Flip()
+        public GateLink(int v, GateLink from, Gate c)
         {
-            Value = !Value;
+            Id = v;
+            _from = from;
+            this.c = c;
+        }
+        public GateLink(int v, GateLink from, GateLink from2)
+        {
+            Id = v;
+            _from = from;
+            _from2 = from2;
         }
 
-        public void Set(bool v)
+        internal bool Calc()
         {
-            Value = v;
+            if (_from != null)
+                if (_from2 != null)
+                    return Nand.Calc(_from.Calc(), _from2.Calc());
+                else if(c != null)
+                    return Nand.Calc(_from.Calc(), c.Value);
+            return Nand.Calc(gate1.Value, gate2.Value);
         }
     }
 }
